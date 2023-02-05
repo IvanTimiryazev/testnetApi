be6051b0d014f7baf8123f52e8b94c56d1fde146
Ivylou <princequincyy@gmail.com>
2023-02-06 01:55:42 +0300

    swagger

--- boot.sh
+++ boot.sh
@@ -8,4 +8,4 @@ while true; do
     echo Upgrade command failed, retrying in 5 secs...
     sleep 5
 done
-exec gunicorn -w 3 -b 0.0.0.0:5005 --access-logfile - --error-logfile - testnet:app
+exec gunicorn -w 3 -b 0.0.0.0:5005 --timeout 600 --access-logfile - --error-logfile - testnet:app
